b5b05090722a83519e7c1c54d2fc9bd1f00eb03b
Jordan Force <jordan.force@uconn.edu>
2018-03-12 13:20:26 -0400

    Pushing up chop.py, which splits up a fasta file into peptides of specified lengths

--- chop.py
+++ chop.py
@@ -8,11 +8,18 @@ if len(sys.argv) > 2:
     j = 0
     for x in sys.argv[2::]:
         lengths.append(int(x))
+    num_proteins = 0
     with open(fasta_file_name, 'r') as handle:
-        for record in SeqIO.parse(handle, "fasta"):
+        for x in SeqIO.parse(handle, 'fasta'):
+            num_proteins += 1
+    with open(fasta_file_name, 'r') as handle:
+        protein_i = 0
+        for record in SeqIO.parse(handle, 'fasta'):
             protein = record.seq
             for length in lengths:
                 for i in range(0, len(protein) - length + 1):
                     print('>' + str(j))
                     j += 1
                     print(protein[i:(i + length)])
+            protein_i += 1
+            print('progress: ' + str(100.0*protein_i/num_proteins), file=sys.stderr)
