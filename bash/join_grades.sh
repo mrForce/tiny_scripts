90f1df29da9c62ad7bf4af23b2fd13b2de9562ab
Jordan Force <jordan.force@uconn.edu>
2018-09-10 18:14:17 -0400

    Added example of usage

--- bash/join_grades.sh
+++ bash/join_grades.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+
+
 spreadsheet=$1
 spreadsheet_grade_column=$2
 huskyct_csv=$3
 huskyct_grade_column=$4
-echo $#
+
 if [ $# -ne 4 ]; then
     echo "Usage: join_grades.sh grades.csv grade_column huskyct.csv huskyct_grade_column. grades.csv is from Google Drive"
+    echo "Example of usage: ./join_grades.sh ~/temp/lab0_spreadsheet.csv 12 ~/Downloads/gc_M1188-CSE-3100-001.011_column_2018-09-10-18-01-41.csv 8"
 else
     spreadsheet_grades=$(mktemp)
     cut -f 1,$spreadsheet_grade_column $spreadsheet | awk '{print $1","$2}'  | sort -t, -k1 > $spreadsheet_grades
