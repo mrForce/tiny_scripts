b8e1644391d1610df3f8eaec1b56eadd8bdd3f56
Jordan Force <jordan.force@uconn.edu>
2019-02-20 14:17:49 -0500

    Whoops, forgot something

--- python/double_linked_list.py
+++ python/double_linked_list.py
@@ -36,12 +36,46 @@ def insert(value):
         iterator = head
         while iterator.value < value:
             iterator = iterator.next_node
+        print('iterator')
+        print(iterator)
+        print(iterator.value)
+        print(iterator.prev_node)
         behind = iterator.prev_node
         next_node = iterator
         behind.next_node = new_node
         new_node.next_node = next_node
         next_node.prev_node = new_node
+        new_node.prev_node = behind
 
+def _delete(value, start):
+    global head
+    global tail
+    if start != None and start.value == value:
+        if start == head:
+            if start == tail:
+                head = None
+                tail = None
+            else:
+                new_head = head.next_node
+                head = new_head
+                head.prev_node = None
+                _delete(value, head)
+        elif start == tail:
+            new_tail = tail.prev_node
+            new_tail.next_node = None
+            tail = new_tail
+        else:
+            new_start = start.next_node
+            start.prev_node.next_node = start.next_node
+            start.next_node.prev_node = start.prev_node
+            _delete(value, new_start)
+def delete(value):
+    global head
+    if head != None:
+        iterator = head
+        while iterator != None and iterator.value < value:
+            iterator = iterator.next_node
+        _delete(value, iterator)
 
         
 insert(1)
@@ -53,4 +87,14 @@ display()
 insert(3)
 display()
 insert(0)
+display()
+insert(3)
+display()
+delete(3)
+display()
+delete(5)
+display()
+delete(1)
+display()
+delete(2)
 display()
