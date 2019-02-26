20e5df9fe1626658c43cf039444fdd124522bb6b
Jordan Force <jordan.force@uconn.edu>
2019-02-26 16:21:17 -0500

    I think I finished the script for adding NetMHC scores and TPM values to the PIN file

--- python/msgf_tpm_correlation/add_tpm_netmhc.py
+++ python/msgf_tpm_correlation/add_tpm_netmhc.py
@@ -5,6 +5,7 @@ Then, convert the Uniprot ID to either Gene name, or RefSeq (depending on what t
 
 """
 import numpy
+import random
 import collections
 import math
 from pyteomics import mzid
@@ -17,6 +18,31 @@ from scipy import stats
 import csv
 import re
 import subprocess
+import tempfile
+NETMHC_LOCATION='/home/jforce/Downloads/netMHC-4.0/Linux_x86_64/bin/netMHC'
+def parse_tpm(path):
+    tpm_data = {}
+    with open(path, 'r') as handler:
+        for line in handler:
+            parts = line.split('\t')
+            if len(parts) == 4:
+                tpm_data[parts[0]] = float(parts[1])
+    return tpm_data
+
+
+#https://www.uniprot.org/help/api_idmapping
+def parse_uniprot_mapper(path, id_type):
+    mapper = collections.defaultdict(list)
+    with open(path, 'r') as handler:
+        for line in handler:
+            parts = line.split('\t')
+            uniprot_id = parts[0].strip()
+            record_type = parts[1].strip()
+            value = parts[2].strip()
+            if record_type == id_type:
+                mapper[uniprot_id].append(value)
+    return mapper
+
 
 """
 Returns a dictionary like this: {'allele': (affinity, rank)...}
@@ -56,47 +82,145 @@ def clean_peptide(seq):
 parser = argparse.ArgumentParser(description='Add TPM values and NetMHC affinity to the output')
 parser.add_argument('mzid_file')
 parser.add_argument('pin_file')
+parser.add_argument('tpm_file')
+parser.add_argument('uniprot_file')
+parser.add_argument('identifier', choices=['RefSeq', 'RefSeq_NT', 'Gene_Name'])
 args = parser.parse_args()
 
+uniprot_data = parse_uniprot_mapper(args.uniprot_file, args.identifier)
+tpm_data = parse_tpm(args.tpm_file)
+"""
+Returns a dictionary mapping the peptide to its affinity
+"""
+def call_netmhc(peptides, hla):
+    file = open('netmhc_input.txt', 'w')
+    output_location = 'netmhc_output.txt'
+    for x in peptides:
+        x.write(x.strip('.-') + '\n')
+    command = [NETMHC_LOCATION, '-a', hla, '-f', 'netmhc_input.txt', '-p', '>', output_location]
+    subprocess.call(command, shell=True)
+    file.close()
+    regex = re.compile('^(\s+[^\s]+){2}(\s+(?P<peptide>[A-Z]+))(\s+[^\s]+){10}(\s+(?P<rank>[0-9]{1,2}\.[0-9]+))')
+    results = {}
+    with open(output_location, 'r') as f:
+        for line in f:
+            match = regex.match(line)
+            if match:
+                peptide = match.group('peptide')
+                affinity = float(match.group(affinity))
+                results[peptide] = affinity
+            else:
+                print('Could not match line: %s' % line)
+    return results
 
 mzid_parser = mzid.DataFrame(args.mzid_file)
 
+netmhc_alleles = ['H-2-Kb']
 
-
-
-for i, row in mzid_parser.iterrows():
-    for i, row in mzid_parser.iterrows():
-        accession = row['accession']
-    if len(accession) == 1 and not row['isDecoy'] and 'MS-GF:RawScore' in row:
-        
-
+"""
+First, extract the targets and decoys from the PIN file. 
+"""
+target_peptides = set()
+decoy_peptides = set()
 
 with open(args.pin_file, 'r') as f:
     reader = csv.DictReader(f, delimiter='\t', restkey='Proteins')
     next(reader)
     for row in reader:
-        scan_number = int(row['ScanNr'])
+        label = row['Label'].strip()
         peptide = clean_peptide(row['Peptide'])
-        all_peptides.add(peptide)
+        if label == '1':
+            target_peptides.add(peptide)
+        elif label == '-1':
+            decoy_peptides.add(peptide)
+        else:
+            assert(label == '-')
 
-    
-peptides_list = list(all_peptides)
+"""
+Call NetMHC on the decoys
+"""
+
+decoy_affinity = {}
+for allele in netmhc_alleles:
+    decoy_affinity[allele] = call_netmhc(decoy_peptides, allele)
 
 
+target_affinity = {}
+target_tpm = {}
 
+"""
+Extract the TPM values and NetMHC affinity from the targets
+"""
+
+
+for i, row in mzid_parser.iterrows():
+    accession = row['accession']
+    if len(accession) == 1 and not row['isDecoy'] and 'MS-GF:RawScore' in row:
+        """
+        The *actual* accessions are seperated via @@. 
+        """
+        accessions = accession[0].split('@@')
+        assert(accessions)
+        tpm = 0
+        for acc in accessions:
+            uniprot_id = acc.split('|')[1]
+            tpm_id_list = uniprot_data[uniprot_id]
+            if len(tpm_id_list) == 0:
+                tpm += 1
+            elif len(tpm_id_list) == 1:
+                tpm_id = tpm_id_list[0]
+                if tpm_id in tpm_data:
+                    tpm += max(1, tpm_data[tpm_id])
+                else:
+                    tpm += 1
+            else:
+                print('more than two tpm ids')
+                print(tpm_id_list)
+                assert(False)
+        netmhc_scores = extract_netmhc_scores(accession[0])
+        peptide = row['PeptideSequence']
+        for allele,scores in netmhc_scores:
+            affinity = float(scores[0])
+            if peptide in target_affinity[allele]:
+                assert(target_affinity[allele][peptide] == affinity)
+            else:
+                target_affinity[allele][peptide] = affinity
+        
+#randomly sample from this to assign NetMHC values to decoys
+tpms = list(target_tpm.values())        
+
+
+"""
+Now go through PIN file, and insert the TPM and affinity
+"""
 pin_output = input('Enter pin output: ')
             
 with open(args.pin_file, 'r') as f:
     reader = csv.DictReader(f, delimiter='\t', restkey='Proteins')
     fieldnames = list(reader.fieldnames)
-    fieldnames.insert(6, 'HMMERScore')
+    fieldnames.insert(6, 'TPM')
+    for allele in netmhc_alleles:
+        fieldnames.insert(7, allele)
     with open(pin_output, 'w') as g:        
         writer = csv.DictWriter(g, fieldnames=fieldnames, delimiter='\t')
         writer.writeheader()
         next(reader)
         for row in reader:
             peptide = clean_peptide(row['Peptide'])
+            label = row['Label']
             row_copy = dict(row)
-            row_copy['HMMERScore'] = peptides_to_hmmer_score[peptide]
+            if label == '1':
+                #target                                
+                assert(peptide in target_tpm)
+                row_copy['TPM'] = target_tpm[peptide]
+                for allele in netmhc_alleles:
+                    assert(peptide in target_affinity[allele])
+                    row_copy[allele] = target_affinity[allele][peptide]
+            elif label == '-1':
+                #decoy, for TPM, just sample randomly
+                row_copy['TPM'] = random.choice(tpms)
+                for allele in netmhc_alleles:
+                    assert(peptide in decoy_affinity[allele])
+                    row_copy[allele] = decoy_affinity[allele][peptide]
             writer.writerow(row_copy)
             
