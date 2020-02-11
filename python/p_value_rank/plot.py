3834ede05c4a352c623d78b2a2492933060abf5f
Jordan Force <jordan.force@uconn.edu>
2020-02-11 10:44:57 -0500

    Adding things

--- python/p_value_rank/plot.py
+++ python/p_value_rank/plot.py
@@ -1,16 +1,16 @@
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import csv
 import math
 import argparse
 
-def plot_pvals(pvals):
+def plot_pvals(pvals, fig, ax):
     #plog log10 of p-value rank on x-axis, and 
     sorted_pvals = sorted(pvals)
     log_pvals = [math.log10(x) for x in sorted_pvals]
     num_pvals = len(log_pvals)
     log_rank_pvals = [math.log10(1.0*j/num_pvals) for j in range(1, num_pvals + 1)]
-    fig, ax = plt.subplots()
     ax.set_xlabel('log10(rank p-value)')
     ax.set_ylabel('log10(p-value)')
     
@@ -23,6 +23,7 @@ def plot_pvals(pvals):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('tide_search_results')
+parser.add_argument('output_image')
 args = parser.parse_args()
 
 tide_search_results = args.tide_search_results
@@ -34,6 +35,9 @@ with open(tide_search_results, 'r') as f:
     pvals = []
     for x in reader:
         pvals.append(float(x['exact p-value']))
-    plot_pvals(pvals)
-    plt.savefig('plot.png')
+    fig = Figure()
+    ax = fig.subplots()
+    
+    plot_pvals(pvals, fig, ax)
+    fig.savefig(args.output_image, format='png')
         
