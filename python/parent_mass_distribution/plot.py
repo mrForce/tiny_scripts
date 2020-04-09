183065de0914ccb1e989ee33a464ab29e96edfa2
Jordan Force <jordan.force@uconn.edu>
2020-04-09 15:47:23 -0400

    Changed the way histograms are displayed

--- python/parent_mass_distribution/plot.py
+++ python/parent_mass_distribution/plot.py
@@ -1,10 +1,11 @@
 import argparse
 from pyteomics import mgf
+import numpy as np
 import matplotlib.pyplot as plt
 import os
 from scipy import stats
 import math
-
+import itertools
 parser = argparse.ArgumentParser(description='Take MGF files, and plot the parent mass. Pass in arbitrary number of files, each of which contains a name and a bunch of MGF file paths to plot the parent masses of.')
 parser.add_argument('plot_dir', help='Where to save the plots')
 parser.add_argument('files', nargs='+', help='Each file contains a name on the first line, and the remaining lines contain paths to MGF files')
@@ -25,6 +26,7 @@ for file_path in args.files:
 
 mass_sets = {}
 kernels = {}
+bin_size = 250
 for name, mgf_set in mgf_sets.items():
     masses = []
     for mgf_file in mgf_set:
@@ -33,32 +35,38 @@ for name, mgf_set in mgf_sets.items():
         mgf_masses = []
         for x in spec_iter:
             mgf_masses.append(x['params']['pepmass'][0]*x['params']['charge'][0])
-        kernel = stats.gaussian_kde(mgf_masses)
-        kernels[mgf_file] = (kernel, name)
-        masses.extend(mgf_masses)
-        """
-        pre, extension = os.path.splitext(mgf_file)
-        png_name = pre + '.png'
-        nums, bins, patches = plt.hist(mgf_masses, label=name, density=True)
-        plt.legend(loc='upper right')
-        plt.savefig(png_name)
-        masses.extend(mgf_masses)
-        plt.clf()
-        """
+        masses.append(mgf_masses)
     mass_sets[name] = masses
 
-min_mass = int(min([min(x) for x in mass_sets.values()]))
-max_mass = math.ceil(max([max(x) for x in mass_sets.values()]))
-
-x_vals = list(range(min_mass, max_mass + 1))
-for name, value in kernels.items():
-    kernel, mgf_set_name = value
-    plt.plot(x_vals, kernel.evaluate(x_vals), label=mgf_set_name)
-    plt.legend(loc='upper right')
-    pre, ext = os.path.splitext(name)
-    png_name = pre + '.png'
-    plt.savefig(os.path.join(args.plot_dir, png_name))
+min_mass = int(min([min(itertools.chain(*x)) for x in mass_sets.values()]))
+max_mass = math.ceil(max([max(itertools.chain(*x)) for x in mass_sets.values()]))
+print('min mass {}'.format(min_mass))
+print('max mass {}'.format(max_mass))
+num_bins = math.ceil((max_mass - min_mass)*1.0/bin_size)
+for name, mgf_set_masses in mass_sets.items():
+    print('plotting')
+    for x in mgf_set_masses:
+        hist, bin_edges = np.histogram(x, bins=num_bins, density=True)
+        bin_centers = 0.5*(bin_edges[1:] + bin_edges[:-1])
+        plt.plot(bin_centers, hist)
+    
+    
+    #bin_centers = 0.5*(bin_edges[1:] + bin_edges[:-1])
+    #plt.plot(bin_centers, hist.shape[0])
+    plt.savefig(os.path.join(args.plot_dir, name + '.png'))
     plt.clf()
+
+for name, mgf_set_masses in mass_sets.items():
+    hist, bin_edges = np.histogram(list(itertools.chain.from_iterable(mgf_set_masses)), bins = num_bins, density=True)
+    bin_centers = 0.5*(bin_edges[1:] + bin_edges[:-1])
+    plt.plot(bin_centers, hist, label=name)
+plt.legend(loc='upper right')
+plt.savefig(os.path.join(args.plot_dir, 'combined.png'))
+"""
+nums, bins, patches = plt.hist(list(itertools.chain.from_iterable(mass_sets.values())), label=list(mass_sets.keys()), density=True, bins=num_bins, histtype='step')
+plt.legend(loc='upper right')
+plt.savefig(os.path.join(args.plot_dir, 'combined.png'))
+
 
 fig, ax = plt.subplots()
 for name, masses in mass_sets.items():
@@ -68,7 +76,6 @@ for name, masses in mass_sets.items():
 
 plt.legend(loc='upper right')
 plt.savefig(os.path.join(args.plot_dir, 'combined.png'))
-"""
 combined_kernels = {k: stats.gaussian_kde(v) for k,v in mass_sets.items()}
 combined_yvals = [(k, v.evaluate(x_vals)) for k,v in combined_kernels.items()]
 plt.plot(x_vals, [x[1] for x in combined_yvals], label=[x[0] for x in combined_yvals])
