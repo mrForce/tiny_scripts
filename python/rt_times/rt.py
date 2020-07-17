7001b612f197cd5a2e8ca174e04fc6caeb67d6e6
Jordan Force <jordan.force@uconn.edu>
2020-07-17 10:03:15 -0400

    Removed FDR filtering. Just do before running this script

--- python/rt_times/rt.py
+++ python/rt_times/rt.py
@@ -6,7 +6,7 @@ parser.add_argument('run_one_mgf', help='MGF file path for run 1')
 parser.add_argument('run_two_mgf', help='MGF file path for run 2')
 parser.add_argument('run_one_percolator', help='Location of percolator.target.peptides.txt file for first run')
 parser.add_argument('run_two_percolator', help='Location of percolator.target.peptides.txt file for second run')
-parser.add_argument('fdr', help='FDR Cutoff to use')
+
 parser.add_argument('output', help='Output file')
 
 args = parser.parse_args()
@@ -23,23 +23,20 @@ def get_rt(file_location):
             retention_times[scan] = rt
     return retention_times
 
-def get_peptides(file_location, fdr):
+def get_peptides(file_location):
     #map each peptide to its scan number.
     peptides = {}
     with open(file_location, 'r') as f:
         tsv_reader = csv.DictReader(f, delimiter='\t')
         for row in tsv_reader:
-            q_value = row['percolator q-value']
-            if float(q_value) <= fdr:
-                scan = row['scan']
-                peptide = row['sequence']
-                assert(peptide not in peptides)
-                peptides[peptide] = scan
+            scan = row['scan']
+            peptide = row['sequence']
+            assert(peptide not in peptides)
+            peptides[peptide] = scan
     return peptides
 
-fdr = float(args.fdr)
-run_one_peptides = get_peptides(args.run_one_percolator, fdr)
-run_two_peptides = get_peptides(args.run_two_percolator, fdr)
+run_one_peptides = get_peptides(args.run_one_percolator)
+run_two_peptides = get_peptides(args.run_two_percolator)
 run_one_rts = get_rt(args.run_one_mgf)
 run_two_rts = get_rt(args.run_two_mgf)
 shared_peptides = run_one_peptides.keys() & run_two_peptides.keys()
